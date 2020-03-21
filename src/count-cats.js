30b09dfed3c2a1109d5fbeb55a418b39f846acf2
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-03-22 00:00:20 +0300

    feat: add cats count function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let cat = '^^';
+  let catsOnBackyard = 0;
+  backyard.forEach(function(el) {
+    el.forEach(function(creature){
+      if (creature == cat) catsOnBackyard++;
+    })
+  });
+  return catsOnBackyard;
 };
