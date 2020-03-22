8adbd2c4ec9b976d441a81d0fa49656bd8068d3d
Denis Khatsuk <dennis.hatsuk@gmail.com>
2020-03-22 17:19:31 +0300

    feat: add extended repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let resultStr = '';
+    str = String(str);
+    let repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+    let separator = options.separator ? options.separator : '+';
+    let addition = (options.addition !== undefined) ? String(options.addition) : '';
+    let additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+    let additionSeparator = options.additionSeparator ? options.additionSeparator : '|';
+    for (let i = 0; i < repeatTimes; i++) {
+        resultStr += str;
+        for (let j = 0; j < additionRepeatTimes; j++) {
+        	if (j !== additionRepeatTimes - 1 ) {
+          	resultStr += addition+additionSeparator;
+          } else {
+          	resultStr += addition;
+          }
+        }
+        if (i !== repeatTimes - 1 ) resultStr += separator;
+    }
+    return resultStr;
 };
   
